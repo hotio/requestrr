399cd60e11ed3f6706266fda0a22d5fa457310a5
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-05-17 20:22:00 +0200

    add __command [skip ci]

--- update-versions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -exuo pipefail
-
-version=$(curl -fsSL --header "Authorization: Bearer ${GITHUB_TOKEN}" "https://api.github.com/repos/thomst08/requestrr/releases/latest" | jq -re .tag_name)
-json=$(cat meta.json)
-jq --sort-keys \
-    --arg version "${version//V/}" \
-    '.version = $version' <<< "${json}" | tee meta.json
